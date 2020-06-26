f7984ec55b8ce1234728033c73c1d63b679906dc
Andrew Selzer <andrew.selzer@americanwell.com>
2020-06-25 20:16:10 -0400

    Add hatching the fox

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,3 +1,6 @@
+import { modFox, modScene } from "./ui";
+import { SCENES, RAIN_CHANCE } from "./constants";
+
 const gameState = {
   current: "INIT",
   clock: 1,
@@ -13,14 +16,17 @@ const gameState = {
     return this.clock;
   },
   startGame() {
-    console.log("hatching");
     this.current = "HATCHING";
     this.wakeTime = this.clock + 2;
+    modFox("egg");
+    modScene("day");
   },
   wake() {
-    console.log("awoken");
     this.current = "IDLING";
     this.wakeTime = -1;
+    modFox("idling");
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
   },
   handleUserAction(icon) {
     // can't do actions while in these states
