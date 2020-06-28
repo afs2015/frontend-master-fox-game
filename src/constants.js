06d38c6a9c65b5a56fa0dff61c6cbcbe79647661
Andrew Selzer <andrew.selzer@americanwell.com>
2020-06-28 18:41:24 -0400

    Updating constants for production environment

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const ICONS = ["fish", "poop", "weather"];
 export const TICK_RATE = 3000;
 export const SCENES = ["day", "rain"];
 export const RAIN_CHANCE = 0.4;
-export const DAY_LENGTH = 3;
-export const NIGHT_LENGTH = 3;
+export const DAY_LENGTH = 60;
+export const NIGHT_LENGTH = 4;
